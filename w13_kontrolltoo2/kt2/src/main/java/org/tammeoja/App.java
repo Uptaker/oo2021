3d849e02325e142c7e06cb9296e8e1ade275635e
Uptaker <markus.tammeoja@gmail.com>
2021-05-07 12:57:47 +0300

    Alustasin 'tähed' KT2 varianti

---
+++ w13_kontrolltoo2/kt2/src/main/java/org/tammeoja/App.java
@@ -0,0 +1,13 @@
+package org.tammeoja;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
