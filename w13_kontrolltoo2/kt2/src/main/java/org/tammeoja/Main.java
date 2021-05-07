1611e40e1194e0703175307e25253345ce22107a
Uptaker <markus.tammeoja@gmail.com>
2021-05-07 14:52:32 +0300

    KT2 progress V6 FINAL

--- w13_kontrolltoo2/kt2/src/main/java/org/tammeoja/Main.java
+++ w13_kontrolltoo2/kt2/src/main/java/org/tammeoja/Main.java
@@ -6,14 +6,14 @@ import java.io.IOException;
 
 // CHECK: Loo liides, mille ainus meetod saab sisendiks tähe ning väljastab selle tähe esinemise arvu.
 // CHECK: Loo realiseeriv klass sõna tarbeks.
-// NOT YET Koosta automaattestid töö kontrolliks näitamaks a, p ja e-tähtede arvu sõnas pere.
+// CHECK: Koosta automaattestid töö kontrolliks näitamaks a, p ja e-tähtede arvu sõnas pere.
 
 // CHECK: Loo liidest realiseeriv klass Lause.
 // CHECK: Lauselt saab lisaks küsida sõnade arvu
 // CHECK: ning Sõna-klassi eksemplaride massiivi. 
 
 // CHECK: Koosta rakendus, kus saab ette anda lause ning väljastatakse a-tähtede arv terves lauses ning igas sõnas eraldi. 
-// NOT YET Koosta automaattest lause a-tähtede arvu kontrolli õigsuse kohta.
+// CHECK: Koosta automaattest lause a-tähtede arvu kontrolli õigsuse kohta.
 // CHECK: Kõik lausetes leidunud eri sõnad lisatakse faili. Rakenduse väljundis näidatakse, millised sõnad on sisendlausest uued ning millised juba varem olemas olnud. 
 
 public class Main {
--- w13_kontrolltoo2/kt2/src/test/java/org/tammeoja/Tests.java
+++ w13_kontrolltoo2/kt2/src/test/java/org/tammeoja/Tests.java
@@ -1,18 +1,41 @@
 package org.tammeoja;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class Tests {
-    /**
-     * Rigorous Test :-)
-     */
+
+    static Word word;
+    static Sentence sentence;
+
+    @BeforeClass
+    public static void init() {
+        word = new Word("pere");
+        sentence = new Sentence("i am a sentence weee look at me me me AAAAA");
+    }
+
+    // Koosta automaattestid töö kontrolliks näitamaks a, p ja e-tähtede arvu sõnas
+    // pere
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void letterExists() {
+        assertEquals(1, word.howManyLetters('p'));
+        assertEquals(2, word.howManyLetters('e'));
+        assertEquals(1, word.howManyLetters('r'));
+    }
+
+    @Test
+    public void letterShouldNotExists() {
+        assertEquals(0, word.howManyLetters('a'));
+        assertEquals(0, word.howManyLetters('b'));
+        assertEquals(0, word.howManyLetters('c'));
+    }
+
+    // Koosta automaattest lause a-tähtede arvu kontrolli õigsuse kohta.
+    @Test
+    public void letterACounterIsCorrect() {
+        assertEquals(8, sentence.howManyALetters());
     }
 }
